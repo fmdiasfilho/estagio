b2c678b30b2784c7ecb9bebe3a23898655922b70
pmdfCelfocus <38101479+pmdfCelfocus@users.noreply.github.com>
2018-07-23 11:55:32 +0100

    Refactoring

--- web/restRequests.js
+++ web/restRequests.js
@@ -1,77 +1,96 @@
-const GET_URL = "http://localhost:9999/";
-const POST_URL = "http://localhost:9999/conversation"; 
+//Rest Server URL
+const SERVER_URL = "http://localhost:9999/";
 
+/**
+ * Creates a XMLHttpResquest and returns it
+ */
 function createRequest() {
-    var result = null;
-    if (window.XMLHttpRequest) {
-      // FireFox, Safari, etc.
-      result = new XMLHttpRequest();
-      if (typeof result.overrideMimeType != 'undefined') {
-        result.overrideMimeType('text/xml'); // Or anything else
-      }
+  var result = null;
+  if (window.XMLHttpRequest) {
+    // FireFox, Safari, etc.
+    result = new XMLHttpRequest();
+    if (typeof result.overrideMimeType != 'undefined') {
+      result.overrideMimeType('text/xml'); // Or anything else
     }
-    else if (window.ActiveXObject) {
-      // MSIE
-      result = new ActiveXObject("Microsoft.XMLHTTP");
-    } 
-    else {
-      // No known mechanism -- consider aborting the application
-    }
-    return result;
   }
-
-  window.onload = function(){
-        var req = createRequest(); 
-        req.open("GET", GET_URL, true);
-        req.send();
-        req.onreadystatechange = function() {
-          if (req.readyState != 4) return;
-          if (req.status != 200) {
-            return;
-          }
-          var resp = req.responseText;
-         generateDiv(resp,true);
-        }
+  else if (window.ActiveXObject) {
+    // MSIE
+    result = new ActiveXObject("Microsoft.XMLHTTP");
   }
+  else {
+    // No known mechanism -- consider aborting the application
+  }
+  return result;
+}
 
-  function XMLPostRequest(){
-    var text = document.getElementById("input").value;
-    generateDiv(text, false);
-    document.getElementById("input").value ='';
-    var req = createRequest();
-    req.open("POST",POST_URL, true);
-    req.setRequestHeader("Content-Type",
-                 "text/plain");
-    var str = String(text);
-    req.send(str);
-    req.onreadystatechange = function() {
+/**
+ * On load function. This code executes when the HTML page is opened 
+ */
+window.onload = function () {
+  var req = createRequest();
+  //Rest server GET method request
+  req.open("GET", SERVER_URL, true);
+  req.send();
+  req.onreadystatechange = function () {
     if (req.readyState != 4) return;
     if (req.status != 200) {
-    return;
+      return;
     }
     var resp = req.responseText;
-    //document.getElementById("response").innerHTML = resp;
     generateDiv(resp, true);
   }
 }
 
-function generateDiv(resp, isBot){
+/**
+ * Creates a POST request to the Rest Server
+ */
+function XMLPostRequest() {
+  //Get input text
+  var text = document.getElementById("input").value;
+  if (text === '') {
+    alert('Empty message!');
+  } else {
+    generateDiv(text, false);
+    document.getElementById("input").value = '';
+    var req = createRequest();
+    req.open("POST", SERVER_URL + 'conversation', true);
+    req.setRequestHeader("Content-Type",
+      "text/plain");
+    var str = String(text);
+    req.send(str);
+    req.onreadystatechange = function () {
+      if (req.readyState != 4) return;
+      if (req.status != 200) {
+        return;
+      }
+      var resp = req.responseText;
+      generateDiv(resp, true);
+    }
+  }
+}
+
+/**
+ * Creates the chat bubble in web page
+ * @param {String} resp 
+ * @param {Boolean} isBot 
+ */
+function generateDiv(resp, isBot) {
   var iDiv = document.createElement('div');
   var para = document.createElement("p");
   para.className = 'chat-message';
   var node = document.createTextNode(resp);
   var element = document.getElementById("logs");
-  if(isBot == true){
+  if (isBot == true) {
     iDiv.className = 'chat bot';
     iDiv.id = 'bot';
-  }else{
+  } else {
     iDiv.className = 'chat self';
     iDiv.id = 'self';
     var order = document.createElement('div');
     order.className = 'blank';
     iDiv.appendChild(order);
   }
- 
+
   para.appendChild(node);
   iDiv.appendChild(para);
   element.appendChild(iDiv);
